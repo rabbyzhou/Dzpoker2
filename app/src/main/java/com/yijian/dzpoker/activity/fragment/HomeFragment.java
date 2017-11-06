788866b9a426ffa1c514a2d83588c1c8c926b7bf
snailGoGoHuang <xianqingdouwh@163.com>
2017-11-06 23:45:35 +0800

    Add MainNewsPager Feature

--- app/src/main/java/com/yijian/dzpoker/activity/fragment/HomeFragment.java
+++ app/src/main/java/com/yijian/dzpoker/activity/fragment/HomeFragment.java
@@ -1,14 +1,29 @@
 package com.yijian.dzpoker.activity.fragment;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.yijian.dzpoker.R;
+import com.yijian.dzpoker.activity.news.DetailNewsActivity;
+import com.yijian.dzpoker.utils.NewsPageUtils;
+import com.yijian.dzpoker.view.data.MainPageNews;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +34,9 @@ import com.yijian.dzpoker.R;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+    private static final String TAG = "HomeFragment";
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,6 +47,65 @@ public class HomeFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private int mDistance;
+
+    private ViewPager mIn_vp;
+    private LinearLayout mIn_ll;
+    private ImageView mLight_dots;
+
+    private ArrayList<MainPageNews> mNewsList= null;
+    private ArrayList<View> mPagerViews = new ArrayList<View>();
+
+    private ViewPagerAdatper mAdapter;
+    public  final static int MESAGE_GETNEWSLIST_OK=0x1001;
+
+    private ProgressDialog mProgressDialog;
+
+    public class ViewPagerAdatper extends PagerAdapter {
+        private ArrayList<View> mViewList ;
+
+        public ViewPagerAdatper(ArrayList<View> mViewList ) {
+            this.mViewList = mViewList;
+        }
+
+        /**
+        private void setList(ArrayList<View> list){
+            this.mViewList = list;
+            notifyDataSetChanged();
+        }**/
+
+        @Override
+        public int getCount() {
+            return mViewList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view==object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, final int position) {
+            View view = mViewList.get(position);
+            container.addView(view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getContext(), DetailNewsActivity.class);
+                    MainPageNews news = mNewsList.get(position);
+                    intent.putExtra("news", news);
+                    getContext().startActivity(intent);
+                }
+            });
+            return mViewList.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(mViewList.get(position));
+        }
+    }
 
     public HomeFragment() {
         // Required empty public constructor
@@ -65,13 +142,183 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        initView(view);
+        initData();
+        return view;
+    }
+
+    private void initView(View inflater){
+        mIn_vp = (ViewPager) inflater.findViewById(R.id.in_viewpager);
+        mIn_ll = (LinearLayout) inflater.findViewById(R.id.in_ll);
+        mLight_dots = (ImageView) inflater.findViewById(R.id.iv_light_dots);
+    }
+
+    private void initData(){
+
+        mNewsList= NewsPageUtils.getNewsList();
+        if ( mNewsList == null || mNewsList.isEmpty()) {
+            QueryDataTask task = new QueryDataTask();
+            task.execute();
+        } else {
+            showData();
+        }
+    }
+
+    private void showData(){
+
+        initPagerViews();
+        mAdapter = new ViewPagerAdatper(mPagerViews);
+        mIn_vp.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
+        addDots();
+        moveDots();
+        mIn_vp.setPageTransformer(true,new DepthPageTransformer());
+    }
+
+    private class QueryDataTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected void onPreExecute() {
+            mProgressDialog = new ProgressDialog(getContext());
+            mProgressDialog.setTitle("请稍后....");
+            mProgressDialog.setMessage("加载中....");
+            mProgressDialog.show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            NewsPageUtils.prepareNewsData(getContext());
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (mProgressDialog.isShowing()){
+                mProgressDialog.cancel();
+            }
+
+            mNewsList= NewsPageUtils.getNewsList();
+            showData();
+            super.onPostExecute(s);
+        }
+    }
+
+    private void initPagerViews(){
+
+        mPagerViews.clear();
+        for(MainPageNews news: mNewsList){
+            Bitmap map = NewsPageUtils.getBitmap(news, getContext());
+            View view = generateView(map);
+            mPagerViews.add(view);
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    private void addDots() {
+
+        for (int i=0; i<mNewsList.size();i++){
+            ImageView dot = new ImageView(getContext());
+            dot.setImageResource(R.drawable.gray_dot);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(0, 0, 40, 0);
+            mIn_ll.addView(dot, layoutParams);
+            final int index = i;
+            dot.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mIn_vp.setCurrentItem(index);
+                }
+            });
+        }
+
+        mLight_dots.setVisibility(View.VISIBLE);
+    }
+
+    private void moveDots() {
+        mLight_dots.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                //获得两个圆点之间的距离
+                if (mIn_ll.getChildCount() > 2) {
+                    mDistance = mIn_ll.getChildAt(1).getLeft() - mIn_ll.getChildAt(0).getLeft();
+                }
+                mLight_dots.getViewTreeObserver()
+                        .removeGlobalOnLayoutListener(this);
+            }
+        });
+        mIn_vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                //页面滚动时小白点移动的距离，并通过setLayoutParams(params)不断更新其位置
+                float leftMargin = mDistance * (position + positionOffset);
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) mLight_dots.getLayoutParams();
+                params.leftMargin = (int) leftMargin;
+                mLight_dots.setLayoutParams(params);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //页面跳转时，设置小圆点的margin
+                float leftMargin = mDistance * position;
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) mLight_dots.getLayoutParams();
+                params.leftMargin = (int) leftMargin;
+                mLight_dots.setLayoutParams(params);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    private View generateView(Bitmap map){
+        if (map == null) return null;
+
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.item_news_page, null);
+        ImageView image = (ImageView)view.findViewById(R.id.news_page);
+        image.setImageBitmap(map);
+        return view;
+    }
+
+    class DepthPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.75f;
+
+        @Override
+        public void transformPage(View page, float position) {
+            int pageWidth = page.getWidth();
+            if (position < -1) { // [-Infinity,-1)
+                // 页面远离左侧页面
+                page.setAlpha(0);
+            } else if (position <= 0) { // [-1,0]
+                // 页面在由中间页滑动到左侧页面 或者 由左侧页面滑动到中间页
+                page.setAlpha(1);
+                page.setTranslationX(0);
+                page.setScaleX(1);
+                page.setScaleY(1);
+            } else if (position <= 1) { // (0,1]
+                //页面在由中间页滑动到右侧页面 或者 由右侧页面滑动到中间页
+                // 淡出效果
+                page.setAlpha(1 - position);
+                // 反方向移动
+                page.setTranslationX(pageWidth * -position);
+                // 0.75-1比例之间缩放
+                float scaleFactor = MIN_SCALE
+                        + (1 - MIN_SCALE) * (1 - Math.abs(position));
+                page.setScaleX(scaleFactor);
+                page.setScaleY(scaleFactor);
+            } else { // (1,+Infinity]
+                // 页面远离右侧页面
+                page.setAlpha(0);
+            }
+
         }
     }
 
---
+++ app/src/main/java/com/yijian/dzpoker/activity/news/DetailNewsActivity.java
@@ -0,0 +1,104 @@
+package com.yijian.dzpoker.activity.news;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.yijian.dzpoker.R;
+import com.yijian.dzpoker.view.data.MainPageNews;
+
+public class DetailNewsActivity extends AppCompatActivity {
+
+    private final String TAG = "DetailNewsActivity";
+    private WebView mWebView;
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_news);
+
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setTitle("请稍后....");
+        mProgressDialog.setMessage("加载中....");
+
+        Intent intent = getIntent();
+        MainPageNews news = (MainPageNews)intent.getSerializableExtra("news");
+
+        TextView titleView = (TextView)findViewById(R.id.title);
+        RelativeLayout backLayout = (RelativeLayout)findViewById(R.id.rl_back);
+        mWebView = (WebView) findViewById(R.id.webView);
+
+        backLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        titleView.setText(news.title);
+
+        Log.d(TAG, "link :" + news.link);
+        WebSettings ws = mWebView.getSettings();
+        ws.setJavaScriptEnabled(true);
+        ws.setAppCacheEnabled(true);
+        final String cachePath = getApplicationContext().getDir("cache", Context.MODE_PRIVATE).getPath();
+        ws.setAppCachePath(cachePath);
+
+        ws.setLoadWithOverviewMode(true);
+        mWebView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                mProgressDialog.show();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                mProgressDialog.hide();
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        mWebView.loadUrl(news.link);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK ){
+            if (mWebView.canGoBack()){
+                mWebView.goBack();
+                return true;
+            } else {
+                finish();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mWebView.destroy();
+        mWebView = null;
+    }
+}
